5ecfd516c02864aae894d6fa587a7f030134521e
Horobets <sairecs1986@gmail.com>
2023-10-07 15:15:25 +0300

    add hw1

---
+++ src/HW1/task4.java
@@ -0,0 +1,19 @@
+package horobets.hw1;
+
+public class main5 {
+    public static void main(String[] args) {
+
+        int a, b;
+        a = 5;
+        b = 3;
+        System.out.println(a);
+        System.out.println(b);
+
+        a = b;
+        System.out.println(a);
+
+        //for (b = a; a-- == b;);
+
+        System.out.println(b);
+    }
+}
