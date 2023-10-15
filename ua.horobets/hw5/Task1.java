1c36753c89fa7c8777c035eb7606f44758d7c040
Horobets <sairecs1986@gmail.com>
2023-10-15 21:45:04 +0300

    add

---
+++ ua.horobets/hw5/Task1.java
@@ -0,0 +1,46 @@
+package org.example.src.ua.horobets.hw5;
+
+import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
+
+/* Заполнение двумерного массива значениями индекса (от 1 и до кол-ва элементов в
+массиве), при этом каждая вторая строка - отрицательными значениями.
+Например
+1 2 3
+-4 -5 -6
+7 8 9
+-10 -11 -12
+*Передать массив в метод, заполнить данный массив */
+public class Task1 {
+    public static void main(String[] args) {
+        int[][] num = new int[4][4];
+        fill(num);
+        changeLine(num);
+        printArray(num);
+
+    }
+    public static void printArray(int[][] array) {
+        for (int i = 0; i < array.length; i++)
+            for (int j = 0; j < array.length; j++)
+                array[i][j] = ThreadLocalRandom.current().nextInt(1, 10);
+        System.out.println(Arrays.deepToString(array));
+    }
+    public static void fill(int[][] num) {
+        int a = 1;
+        for (int i = 0; i < num.length; i++) {
+            for (int j = 0; j < num.length; j++) {
+                num[i][j] = a + 1;
+                System.out.println(Arrays.deepToString(num));
+            }
+        }
+    }
+    public static void changeLine(int[][] num) {
+        for (int i = 0; i < num.length; i++) {
+            for (int j = 0; j < num[i].length; j++) {
+                if (i % 2 != 0) {
+                    num[i][j] *= (-1);
+                }
+            }
+        }
+    }
+}
