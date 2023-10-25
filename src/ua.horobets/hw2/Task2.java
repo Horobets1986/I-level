bee6afc6b0cae3f477b156f73b3df005247263a4
Horobets <sairecs1986@gmail.com>
2023-10-26 00:42:44 +0300

    add

--- src/ua.horobets/hw2/Task2.java
+++ src/ua.horobets/hw2/Task2.java
@@ -1,6 +1,7 @@
 package ua.horobets.hw2;
 /* 3. Определить является ли переданное в метод число четным или нет и вывести результат в
 консоль. */
+
 import java.util.Scanner;
 
 public class Task2 {
@@ -11,9 +12,10 @@ public class Task2 {
         int num = scan.nextInt();
         nymber(num);
     }
-        public static void nymber(int num) {
 
-        if ((num%2) > 0) {
+    public static void nymber(int num) {
+
+        if ((num % 2) > 0) {
             System.out.println("number is not pair");
         } else {
             System.out.println("number is pair");
