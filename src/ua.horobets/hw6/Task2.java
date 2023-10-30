56a2e4123f22bca7a35572fed1545f9fadd15355
Horobets <sairecs1986@gmail.com>
2023-10-30 23:41:21 +0200

    fix

--- src/ua.horobets/hw6/Task2.java
+++ src/ua.horobets/hw6/Task2.java
@@ -1,5 +1,15 @@
 package ua.horobets.hw6;
-
+/*
+запустив компілятор щоб визначити тип помилки
+показало Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: Index 1 out of bounds for length 1
+це значає що ми виходимо за межі масива
+виставив point на переботах масива і став рухатись по операційно до моменту помилки
+знаходжу що в 30 рядку  for (int j = 0; j < triangle[i].length; j++) помилка не правильно заданий масив
+замінив [i] на тому що він не виходить за межі даного масива [j]
+і зайшов помилку у виводі System.out.println(triangle[i].length);
+та переніс в правильне місце
+запустив компілятор помилки більше не було
+ */
 public class Task2 {
     public static void main(String[] args) {
         int[][] triangle = new int[5][];
