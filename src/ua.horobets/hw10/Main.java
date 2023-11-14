8aa8626f141ea861c02c94979431c587dd642277
Horobets <sairecs1986@gmail.com>
2023-11-14 20:59:21 +0200

    add exception to main

--- src/ua.horobets/hw10/Main.java
+++ src/ua.horobets/hw10/Main.java
@@ -4,10 +4,35 @@ Add parameters validity check into Figure HW
 If parameters are not valid (negative or zero value, non existence triangle) you should throw custom exceptions
  */
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws InvalidFigureException {
-        Figure Circle = new Circle(8);
-        Figure Square = new Square(9);
-        Figure Triangle = new Triangle(6, 5, 9);
+        System.out.println("enter radius of circle");
+        Scanner scanner = new Scanner(System.in);
+        double radius = scanner.nextDouble();
+        Figure circle = new Circle(radius);
+
+        System.out.println("enter side of square");
+        double side = scanner.nextDouble();
+        Figure Square = new Square(side);
+
+        System.out.println("enter side of triangle");
+        double sideA = scanner.nextDouble();
+        double sideB = scanner.nextDouble();
+        double sideC = scanner.nextDouble();
+        Figure Triangle = new Triangle(sideA, sideB, sideC);
+
+        while (true) {
+            try {
+                if ((sideA + sideB < sideC) || (sideA + sideC < sideB) || (sideB + sideC < sideA)) {
+                    throw new InvalidFigureException(" Triangle does exist");
+                } else break;
+
+            } catch (InvalidFigureException e) {
+                throw new RuntimeException(e);
+
+            }
+        }
     }
 }
