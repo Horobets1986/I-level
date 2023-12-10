d2a26152debfb2e3ac9a6e79a721a40e9a8fccfd
Horobets <sairecs1986@gmail.com>
2023-12-10 18:44:43 +0200

    add reflection

---
+++ src/ua.horobets/hw15/ShowInfo.java
@@ -0,0 +1,12 @@
+package ua.horobets.hw15;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ShowInfo {
+    boolean showInfo() default true;
+}
