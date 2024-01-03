a86ee5840b776e41628aee92f482dd978ea9a720
Horobets <sairecs1986@gmail.com>
2024-01-04 00:25:23 +0200

    add JSON

---
+++ src/main/java/ua.horobets/hw18/Person.java
@@ -0,0 +1,14 @@
+package ua.horobets.hw18;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+    private String name;
+    private Integer age;
+    private String hobby;
+}
